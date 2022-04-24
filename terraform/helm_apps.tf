8ed2eeadd57b804c940683d2bd4fd0e965430798
Joshua Tonga <hellojoshuatonga@gmail.com>
2022-04-25 06:10:53 +0800

    build(terraform): add terraform setup for k8s cluster components

---
+++ terraform/helm_apps.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "extraArgs.ingress-class"
+    value = "nginx"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
+
